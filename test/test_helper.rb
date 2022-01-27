2e4a71b9351a360bc2e4ee114f1a4d7f097b1b5b
Brice Texier <brice@codeur.com>
2022-01-27 18:06:30 +0100

    Fix coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-require 'rubocop/codeur'
-
 if ENV['COVERAGE']
   require 'simplecov'
   SimpleCov.start
 end
+
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'rubocop-codeur'
 
 require 'minitest/autorun'
